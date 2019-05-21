bf991526b67aef1a76285998ecab4e6d25defece
Harsh Soni <harshsoni111093@gmail.com>
2019-05-21 21:14:11 +0530

    Feat: Navigation between screen.

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/TrialLayout.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+          initialRoute: '/',
+          routes: {
+            '/' : (context) => FirstScreen(),
+            '/layout' : (context) => MyApp()
+          },
+          title: "Flutter Trials",
+        ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Welcome to flutter",
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Flutter layout demo'),
-            ),
-            body: TrialLayout()));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: TrialLayout());
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Trials"),
+        ),
+        body: Center(
+          child: RaisedButton(
+              child: Text("Open layout"),
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  '/layout',
+                );
+              }),
+        ));
   }
 }
